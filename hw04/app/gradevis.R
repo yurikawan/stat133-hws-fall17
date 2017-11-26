3060218cc0cd07a1a1bd8211a417efd072fe335b
yurikawan <31088302+yurikawan@users.noreply.github.com>
2017-11-26 01:22:02 -0800

    Update gradevis.R

--- hw04/app/gradevis.R
+++ hw04/app/gradevis.R
@@ -14,26 +14,26 @@ proportion <- prop.table(table(raw_scores$Grade))
 helper_table <- data.frame(grade)
 helper_table$Freq <- frequency
 helper_table$Prop <- round(proportion,2)
-helper_table <- helper_table[c(2,1,3,5,4,6,8,7,9,10,11),]
+helper_table$grade <- factor(helper_table$grade, levels=c("A+", "A", "A-", "B+", "B", "B-", "C+","C", "C-", "D", "F"))
 
 install.packages("shiny")
 library(shiny)
 
 ui <- shinyUI(pageWithSidebar(
-  headerPanel("Grade Distribution"),
-    sidebarPanel(conditionalPanel(condition = "input.tabselected==1",dataTableOutput(outputId="distribution")),
+  headerPanel("Grade Visualizer"),
+    sidebarPanel(conditionalPanel(condition = "input.tabselected==1",h3("Grades Distribution"), tableOutput(outputId="distribution")),
       conditionalPanel(condition = "input.tabselected==2",selectInput(inputId="sel_1",label="X-axis variable",choices=select_from),sliderInput(inputId = "bin",label = "Bin Width",min=1,max=10,value=5)),
       conditionalPanel(condition = "input.tabselected==3",selectInput(inputId="sel_2",label ="X-axis variable",choices=select_from),selectInput(inputId='sel_3',label="Y-axis variable",choices=select_from),sliderInput(inputId="opacity",label="Opacity",min=0,max=1,step=0.1,value=0.5), radioButtons(inputId="method",label="Show line",choices=c("none","lm","loess")))
     ), mainPanel(tabsetPanel
             (tabPanel("Barchart",value=1,uiOutput("bar1"),ggvisOutput("bar1_ui")),
-              tabPanel("Histogram",value=2, uiOutput("hist1"), ggvisOutput("hist1-ui"),verbatimTextOutput("summary")),
-              tabPanel("Scatterplot",value=3,uiOutput("scatter1"),ggvisOutput("scatter1_ui"), verbatimTextOutput("cor")),id = "tabselected"))
+              tabPanel("Histogram",value=2, uiOutput("hist1"), ggvisOutput("hist1-ui"),h3("Summary Statistics:"),verbatimTextOutput("summary")),
+              tabPanel("Scatterplot",value=3,uiOutput("scatter1"),ggvisOutput("scatter1_ui"), h3("Correlation:"), verbatimTextOutput("cor")),id = "tabselected"))
 ))
 
 
 
 server <- function(input,output){
-  output$distribution <- renderDataTable({helper_table})
+  output$distribution <- renderTable({helper_table})
   helper_table %>% ggvis(~grade,~Freq) %>% layer_bars(fill := "lightblue",width=0.8,stroke := "lightblue") %>% add_axis("y",title="frequency") %>% bind_shiny("bar1","bar1_ui")
   hist_plot <- reactive({
     xvar <- prop("x", as.symbol(input$sel_1))
